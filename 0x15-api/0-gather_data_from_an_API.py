9c13c93d0b21f71c2f1a7f4fdb77830fe395d458
Jorshstar <Jorshstar>
2023-04-25 17:29:46 -0700

    Api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,7 +3,6 @@
 import requests
 import sys
 
-
 if len(sys.argv) < 2:
     print("Usage: {} employee_id".format(sys.argv[0]))
     sys.exit(1)
@@ -11,12 +10,12 @@ if len(sys.argv) < 2:
 employee_id = sys.argv[1]
 base_url = 'https://jsonplaceholder.typicode.com/'
 
-# Script to Fetch  user information
+# Fetch the user information
 user_response = requests.get(base_url + 'users/{}'.format(employee_id))
 user_data = user_response.json()
 employee_name = user_data['name']
 
-# Script to Fetch  user's todo list
+# Fetch the user's todo list
 todo_response = requests.get(base_url + 'todos?userId={}'.format(employee_id))
 todo_data = todo_response.json()
 
@@ -30,8 +29,8 @@ for task in todo_data:
         num_completed_tasks += 1
         completed_tasks.append(task['title'])
 
-# Script to Print the output in the specified format
-print("Employee {} is done with tasks({}/{}):"
-        .format(employee_name, num_completed_tasks, num_total_tasks))
+# Print the output in the specified format
+print("Employee {} is done with tasks({}/{}):".format(
+    employee_name, num_completed_tasks, num_total_tasks))
 for task in completed_tasks:
     print("\t {}".format(task))
