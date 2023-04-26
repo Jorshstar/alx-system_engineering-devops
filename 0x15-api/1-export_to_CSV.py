d4ad92f1c9b71c450112f8e619aa5052b7acbd44
Jorshstar <Jorshstar>
2023-04-25 17:37:53 -0700

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
-
+"""Python Scripts that Exports to-do list for a given employee ID to CSV format."""
+import csv
 import requests
 import sys
-import csv
 
-if len(sys.argv) < 2:
-    print("Usage: {} employee_id".format(sys.argv[0]))
-    sys.exit(1)
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-employee_id = sys.argv[1]
-base_url = 'https://jsonplaceholder.typicode.com/'
-
-# Fetch the user information
-user_response = requests.get(base_url + 'users/{}'.format(employee_id))
-user_data = user_response.json()
-employee_name = user_data['username']
-
-# Fetch the user's todo list
-todo_response = requests.get(base_url + 'todos?userId={}'.format(employee_id))
-todo_data = todo_response.json()
-
-# Prepare the data for export
-data = []
-for task in todo_data:
-    task_title = task['title']
-    task_completed = task['completed']
-    data.append([employee_id, employee_name, task_completed, task_title])
-
-# Export the data in CSV format
-filename = '{}.csv'.format(employee_id)
-with open(filename, 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
-    for row in data:
-        writer.writerow(row)
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
