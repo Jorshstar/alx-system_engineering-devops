36141b9ccc9f8bd91dace3872838e97e3ccbb506
Jorshstar <Jorshstar>
2023-04-25 17:41:14 -0700

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Python Scripts that Exports to-do list for a given employee ID to CSV format."""
+
+"""Exports to-do list information for a given employee ID to CSV format."""
+
 import csv
 import requests
 import sys
+
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
@@ -13,6 +16,4 @@ if __name__ == "__main__":
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+        [writer.writerow([user_id, username, t.get("completed"), t.get("title")]) for t in todos]
