1511a2cf4664d96582ee468efc832186362e6dc9
Jorshstar <Jorshstar>
2023-04-25 17:22:52 -0700

    Api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 
-# Write a Python script that
-#     using this REST API, for a given employee ID
-#     returns information about his/her Todo list progress
-
 import requests
 import sys
+
 
 if len(sys.argv) < 2:
     print("Usage: {} employee_id".format(sys.argv[0]))
     sys.exit(1)
 
 employee_id = sys.argv[1]
-    base_url = 'https://jsonplaceholder.typicode.com/'
+base_url = 'https://jsonplaceholder.typicode.com/'
 
-# script that fetch user information
+# Script to Fetch  user information
 user_response = requests.get(base_url + 'users/{}'.format(employee_id))
 user_data = user_response.json()
 employee_name = user_data['name']
 
-# Script that fetch user's todo list
-todo_response = requests.get(base_url + 'todos?userId={}'
-        .format(employee_id))
+# Script to Fetch  user's todo list
+todo_response = requests.get(base_url + 'todos?userId={}'.format(employee_id))
 todo_data = todo_response.json()
 
-# script that Count the number of completed tasks and the total number of tasks
+# Count the number of completed tasks and the total number of tasks
 num_completed_tasks = 0
 num_total_tasks = len(todo_data)
 completed_tasks = []
@@ -34,8 +30,7 @@ for task in todo_data:
         num_completed_tasks += 1
         completed_tasks.append(task['title'])
 
-# script that Print the output in the specified format
-print("Employee {} is done with tasks({}/{}):"
-        .format(employee_name, num_completed_tasks, num_total_tasks))
-    for task in completed_tasks:
+# Script to Print the output in the specified format
+print("Employee {} is done with tasks({}/{}):".format(employee_name, num_completed_tasks, num_total_tasks))
+for task in completed_tasks:
     print("\t {}".format(task))
